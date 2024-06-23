41b84b5309d20abe1bae143f862c61576ef4d621
Mohamed Hesham <20201701650@cis.asu.edu.eg>
2024-06-23 23:36:55 +0300

    TripDetailScreen Improved

--- lib/firebasemetro/tripdetailsScreen.dart
+++ lib/firebasemetro/tripdetailsScreen.dart
@@ -14,9 +14,33 @@ class TripDetails extends StatefulWidget {
 class _TripDetailsState extends State<TripDetails> {
   Color cardColor = Colors.white;
   Color textColor = Colors.black;
-  Color appBarColor = Color(0xFF073042);
+    Color appBarColor = Color(0xFF073042);
   Color accentColor = Color(0xFF00796B);
   Color errorColor = Color(0xFFB31312);
+  final GlobalKey part1Key = GlobalKey();
+  final GlobalKey descKey = GlobalKey();
+  final GlobalKey appbarKey = GlobalKey();
+  final GlobalKey dividerKey = GlobalKey();
+  late double _widgetHeight;
+  late double _deskHeight;
+  //late double _dividerHeight;
+  //late double _appbarHeight;
+  late double _screenHeight; // Get the ratio to set as max size.
+  late double requiredHeight = 0.5;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _widgetHeight = getWidgetHeight(part1Key);
+      _deskHeight = getWidgetHeight(descKey);
+      //_dividerHeight = getWidgetHeight(dividerKey);
+      //_appbarHeight = getWidgetHeight(appbarKey);
+      _screenHeight = screenHeight(context);
+      requiredHeight = 0.93 - (((_widgetHeight + _deskHeight) / _screenHeight));
+      setState(() {}); // Update the UI with the new height
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +50,7 @@ class _TripDetailsState extends State<TripDetails> {
       backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
+        key: appbarKey,
         title: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
@@ -35,7 +60,7 @@ class _TripDetailsState extends State<TripDetails> {
                       color: Colors.white, fontSize: screenWidth * 0.05)),
               const Text('  To  ',
                   style: TextStyle(
-                      color: Colors.white,
+                      color: Color.fromARGB(255, 255, 255, 255),
                       fontWeight: FontWeight.bold)),
               Text(widget.route.routeStations.last,
                   style: TextStyle(
@@ -43,78 +68,86 @@ class _TripDetailsState extends State<TripDetails> {
             ],
           ),
         ),
-        backgroundColor: appBarColor,
+        backgroundColor: const Color(0xFF073042),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
+        children: [
+          Column(
             children: [
-              TripDescription(
-                widget.route.line,
-                widget.route.direction,
-                widget.route.transit,
-                screenWidth,
+              ///////////////////////////////////////////////////////////////////////////
+              //Part1
+              Padding(
+                key: descKey,
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: TripDescription(
+                      widget.route.line,
+                      widget.route.direction,
+                      widget.route.transit,
+                      screenWidth),
+                ),
               ),
-              SizedBox(height: 10),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Container(
-                      width: screenWidth * 0.4,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            spreadRadius: 0.5,
-                            blurRadius: 4,
-                          )
-                        ],
-                        color: cardColor,
-                      ),
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.subway,
-                            size: screenWidth * 0.12,
-                            color: accentColor,
-                          ),
-                          Text(
-                            'Stations',
-                            style: TextStyle(
-                              color: accentColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: screenWidth * 0.04,
+              Padding(
+                key: part1Key,
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: screenWidth * 0.4,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              spreadRadius: 0.5,
+                              blurRadius: 4,
+                            )
+                          ],
+                          color: cardColor,
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.subway,
+                              size: screenWidth * 0.12,
+                              color: Color(0xFF00796B),
                             ),
-                          ),
-                          SizedBox(
-                            width: screenWidth * 0.15,
-                            height: screenWidth * 0.08,
-                            child: Center(
-                              child: Text(
-                                widget.route.routeStations.length.toString(),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: screenWidth * 0.05,
-                                  color: textColor,
+                            Text(
+                              'Stations',
+                              style: TextStyle(
+                                color: Color(0xFF00796B),
+                                fontWeight: FontWeight.bold,
+                                fontSize: screenWidth * 0.04,
+                              ),
+                            ),
+                            SizedBox(
+                              width: screenWidth * 0.15,
+                              height: screenWidth * 0.08,
+                              child: Center(
+                                child: Text(
+                                  widget.route.routeStations.length.toString(),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: screenWidth * 0.05,
+                                    color: textColor,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  if (widget.route.transit != '')
-                    SizedBox(width: 10),
-                  if (widget.route.transit != '')
-                    Expanded(
-                      child: Container(
+                    if (widget.route.transit != '')
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    if (widget.route.transit != '')
+                      Container(
                         decoration: BoxDecoration(
                           boxShadow: [
                             BoxShadow(
@@ -131,14 +164,14 @@ class _TripDetailsState extends State<TripDetails> {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             Icon(
+                              color: Color(0xFF00796B),
                               Icons.transit_enterexit_outlined,
                               size: screenWidth * 0.12,
-                              color: accentColor,
                             ),
                             Text(
                               'Interchange Station',
                               style: TextStyle(
-                                color: accentColor,
+                                color: Color(0xFF00796B),
                                 fontWeight: FontWeight.bold,
                                 fontSize: screenWidth * 0.04,
                               ),
@@ -158,45 +191,103 @@ class _TripDetailsState extends State<TripDetails> {
                           ],
                         ),
                       ),
-                    ),
-                ],
+                  ],
+                ),
               ),
+
+              ///end of part1
+              ////////////////////////////////////////////////////////////////////////
               const Divider(
-                color: Colors.black26,
+                color: Colors.black,
                 endIndent: 10,
                 indent: 10,
               ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: widget.route.routeStations.length,
-                itemBuilder: (context, index) {
-                  bool isTransit = widget.route.routeStations[index] ==
+            ],
+          ),
+          ///////////////////////////////////////////////////////////////////////////
+          ///part2
+          DraggableScrollableSheet(
+            initialChildSize: requiredHeight.clamp(0.1, 1.0),
+            //kol ma el rakb bykbr bntl3 l fo2
+            minChildSize: requiredHeight.clamp(0.1, 1.0),
+            //dh el hyt8yr w lazm yb2 a2l mn el tani
+            builder: (context, scrollController) {
+              return Container(
+                decoration: const BoxDecoration(color: Color(0xFF073042)),
+                child: ListView.builder(
+                  physics: const ClampingScrollPhysics(),
+                  controller: scrollController,
+                  shrinkWrap: true,
+                  itemCount: widget.route.routeStations.length,
+                  itemBuilder: (context, index) {
+                     bool isTransit = widget.route.routeStations[index] ==
                       widget.route.transit;
                   bool isFirst = index == 0;
                   bool isLast = index == widget.route.routeStations.length - 1;
                   bool afterTransit = widget.route.transit != '' &&
                       index > widget.route.routeStations.indexOf(widget.route.transit);
 
-                  return Padding(
-                    padding: EdgeInsets.only(
-                      left: isTransit || afterTransit ? 60.0 : 0.0,
-                      bottom: 10.0,
-                    ),
-                    child: StationCard(
+                    if (index == 0) {
+                      return Column(
+                        children: [
+                          const SizedBox(
+                            width: 50,
+                            child: Divider(
+                              thickness: 5,
+                            ),
+                          ),
+                          const Text(
+                            'Stations',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                          StationCard(
                       stationName: widget.route.routeStations[index],
                       isTransit: isTransit,
                       isFirst: isFirst,
                       isLast: isLast,
                     ),
-                  );
-                },
-              ),
-            ],
+                        ],
+                      );
+                    }
+
+                    return StationCard(
+                      stationName: widget.route.routeStations[index],
+                      isTransit: isTransit,
+                      isFirst: isFirst,
+                      isLast: isLast,
+                    );
+                  },
+                ),
+              );
+            },
           ),
-        ),
+
+          /// end of part2
+          ///////////////////////////////////////////////////////////////////////
+        ],
       ),
     );
+  }
+
+  double Draggablenumber() {
+    double extra = 0.03;
+    if (widget.route.direction[0] == 'Shoubra El-Kheima Direction' ||
+        widget.route.direction[1] == 'Shoubra El-Kheima Direction' ||
+        widget.route.direction[1] == 'Rod El Farag Corridor Direction' ||
+        widget.route.direction[0] == 'Rod El Farag Corridor Direction' ||
+        widget.route.direction[1] == 'Adli Mansour Direction' ||
+        widget.route.direction[0] == 'Adli Mansour Direction' ||
+        widget.route.transit == 'Al-Shohada') {
+      extra = 0.00;
+    }
+    if (widget.route.transit == '') {
+      return 0.67 - extra;
+    } else {
+      return 0.6 - extra;
+    }
   }
 
   Widget TripDescription(List<int> line, List<String> direction, String transit,
@@ -208,7 +299,7 @@ class _TripDetailsState extends State<TripDetails> {
             boxShadow: [
               BoxShadow(
                 color: Colors.black.withOpacity(0.3),
-                spreadRadius:0.5,
+                spreadRadius: 0.5,
                 blurRadius: 5,
               )
             ],
@@ -223,7 +314,7 @@ class _TripDetailsState extends State<TripDetails> {
               Text(
                 'Trip Description:',
                 style: TextStyle(
-                    color: accentColor,
+                    color: Color(0xFF00796B),
                     fontSize: screenWidth * 0.05,
                     fontWeight: FontWeight.bold),
               ),
@@ -255,6 +346,17 @@ class _TripDetailsState extends State<TripDetails> {
       ],
     );
   }
+
+  double getWidgetHeight(GlobalKey k) {
+    final RenderBox renderBox =
+        k.currentContext?.findRenderObject() as RenderBox;
+    return renderBox.size.height;
+  }
+
+  double screenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+  
 }
 
 class StationCard extends StatelessWidget {
@@ -284,7 +386,7 @@ class StationCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -336,14 +438,14 @@ class VerticalDividerLine extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 2,
-      height: 30,
+      height: 20,
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.grey, Colors.white],
+          colors: [Colors.black,Color(0xFF00796B) ],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         ),
       ),
     );
   }
-}
+  }
